95248c648747719a1a496c2afb70b05348aa0c5c
J RP <j.ruizperez@dundee.ac.uk>
2020-04-04 18:43:04 +0200

    Full rest API

--- alien-1/src/main/java/com/example/alien/controller/AlienController.java
+++ alien-1/src/main/java/com/example/alien/controller/AlienController.java
@@ -5,16 +5,22 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.alien.dao.AlienRepo;
 import com.example.alien.model.Alien;
 
-@Controller
+@RestController
 public class AlienController 
 {	
 	@Autowired
@@ -24,32 +30,50 @@ public class AlienController
 	{
 		return "home.jsp";
 	}
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
+	
+	@PutMapping("/alien")
+	public Alien updateAlien(@RequestBody Alien alien) {
 		repo.save(alien);
-		return "home.jsp";
+		return alien;
 	}
+	
+	@DeleteMapping("/alien/{name}")
+	public String deleteAlien(@PathVariable String name)
+	{
+		Alien a = repo.getOne(name);
+		repo.delete(a);
+		return "deleted";
+	}
+	
+	@PostMapping("/alien")
+	public Alien addAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+	
+	@GetMapping("/aliens")
+	public List<Alien> getAliens() {
+		return repo.findAll();
+	}
+	
+	@RequestMapping("/alien/{name}")
+	public Optional<Alien> oneAlien(@PathVariable("name") String name) {
+		return repo.findById(name);
+	}
+	
+	// old
 	@RequestMapping("/deleteAlien")
-	public String deleteAlien(String name) {
+	public String deleteOneAlien(String name) {
 		repo.deleteById(name);
 		return "home.jsp";
 	}
+	
 	@RequestMapping("/getAlien")
 	public ModelAndView getAlien(@RequestParam String name) {
 		ModelAndView mv= new ModelAndView("showAlien.jsp");
 		Alien alien = repo.findById(name).orElse(new Alien());
 		mv.addObject(alien);
 		return mv;
-	}
-	@RequestMapping("/aliens")
-	@ResponseBody
-	public List<Alien> getAliens() {
-		return repo.findAll();
-	}
-	@RequestMapping("/alien/{name}")
-	@ResponseBody
-	public Optional<Alien> oneAlien(@PathVariable("name") String name) {
-		return repo.findById(name);
 	}
 	
 }
