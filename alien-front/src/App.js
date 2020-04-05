ee10396a18a5679f40f10ff22be85056674dc2a2
J RP <j.ruizperez@dundee.ac.uk>
2020-04-05 18:30:44 +0200

    Add, edit and delete methods are now supported in front interface

--- alien-front/src/App.js
+++ alien-front/src/App.js
@@ -12,14 +12,17 @@ class App extends Component {
       parent:''
 
     },
-    newAlienModal: false
+    editAlienData: {
+      name:'',
+      type:'',
+      planet:'',
+      parent:''
+    },
+    newAlienModal: false,
+    editAlienModal: false
   }
   componentWillMount() {
-    axios.get('http://localhost:8080/aliens').then((response) => {
-      this.setState({
-        aliens: response.data
-      })
-    });
+    this._refreshAliens()
   }
 
   addAlien(){
@@ -36,10 +39,41 @@ class App extends Component {
       }});
     });
   }
+  updateAlien() {
+    axios.put('http://localhost:8080/alien', this.state.editAlienData).then((response) => {
+      this._refreshAliens();
 
+      this.setState({
+        editAlienModal: false, editAlienData: {name: '', type: '', planet: '', parent: ''}
+      });
+    });
+  }
+  editAlien(name, type, planet, parent) {
+    this.setState( {
+      editAlienData: { name, type, planet, parent}, editAlienModal: ! this.state.editAlienModal
+    });
+  }
+  _refreshAliens () {
+    axios.get('http://localhost:8080/aliens').then((response) => {
+      this.setState({
+        aliens: response.data
+      })
+    });
+  }
+  deleteAlien (name) {
+      axios.delete('http://localhost:8080/alien/' + name).then((response) => {
+        this._refreshAliens();
+      });
+  }
   toggleNewAlienModal() {
     this.setState({
       newAlienModal: ! this.state.newAlienModal
+    })
+    // this.state.newAlienModal  = true;
+  }
+  toggleEditAlienModal() {
+    this.setState({
+      editAlienModal: ! this.state.editAlienModal
     })
     // this.state.newAlienModal  = true;
   }
@@ -52,8 +86,8 @@ class App extends Component {
         <td>{alien.planet}</td>
         <td>{alien.parent}</td>
         <td>
-          <Button color="success" size="sm" className="mr-2">Edit</Button>
-          <Button color="danger" size="sm">Delete</Button>
+          <Button color="success" size="sm" className="mr-2" onClick={this.editAlien.bind(this, alien.name, alien.type, alien.planet, alien.parent)}>Edit</Button>
+          <Button color="danger" size="sm" onClick={this.deleteAlien.bind(this, alien.name)}>Delete</Button>
         </td>
       </tr>
       )
@@ -61,7 +95,10 @@ class App extends Component {
     return (
       <div className="App container">
 
-      <Button color="primary" onClick={this.toggleNewAlienModal.bind(this)}>Add a new alien</Button>
+      <h1> Alien race App</h1>
+
+      <Button className="my-3" color="primary" onClick={this.toggleNewAlienModal.bind(this)}>Add a new alien</Button>
+
       <Modal isOpen={this.state.newAlienModal} toggle={this.toggleNewAlienModal.bind(this)}>
         <ModalHeader toggle={this.toggleNewAlienModal.bind(this)}>Add a new alien</ModalHeader>
         <ModalBody>
@@ -101,6 +138,48 @@ class App extends Component {
         <ModalFooter>
           <Button color="primary" onClick={this.addAlien.bind(this)}>Add Alien</Button>{' '}
           <Button color="secondary" onClick={this.toggleNewAlienModal.bind(this)}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={this.state.editAlienModal} toggle={this.toggleEditAlienModal.bind(this)}>
+        <ModalHeader toggle={this.toggleEditAlienModal.bind(this)}>Edit an alien</ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input id="name" value={this.state.editAlienData.name} onChange={(e) => {
+              let {editAlienData} = this.state
+              editAlienData.name = e.target.value;
+              this.setState({editAlienData});
+            }}/>
+          </FormGroup>
+          <FormGroup>
+            <Label for="type">Type</Label>
+            <Input id="type" value={this.state.editAlienData.type} onChange={(e) => {
+              let {editAlienData} = this.state
+              editAlienData.type = e.target.value;
+              this.setState({editAlienData});
+            }}/>
+          </FormGroup>
+          <FormGroup>
+            <Label for="planet">Birth Planet</Label>
+            <Input id="planet" value={this.state.editAlienData.planet} onChange={(e) => {
+              let {editAlienData} = this.state
+              editAlienData.planet = e.target.value;
+              this.setState({editAlienData});
+            }}/>
+          </FormGroup>
+          <FormGroup>
+            <Label for="parent">Parent</Label>
+            <Input id="parent" value={this.state.editAlienData.parent} onChange={(e) => {
+              let {editAlienData} = this.state
+              editAlienData.parent = e.target.value;
+              this.setState({editAlienData});
+            }}/>
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.updateAlien.bind(this)}>Edit Alien</Button>{' '}
+          <Button color="secondary" onClick={this.toggleEditAlienModal.bind(this)}>Cancel</Button>
         </ModalFooter>
       </Modal>
 
